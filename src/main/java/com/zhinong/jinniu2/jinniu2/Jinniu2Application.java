d720121e2d6e3fe00c9f4dff41c7f3b07c1b73c3
qiaolei <qiaolei@uyplay.cn>
2020-11-18 18:12:40 +0800

    项目创建

---
+++ src/main/java/com/zhinong/jinniu2/jinniu2/Jinniu2Application.java
@@ -0,0 +1,13 @@
+package com.zhinong.jinniu2.jinniu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jinniu2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jinniu2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhinong/jinniu2/jinniu2/Jinniu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhinong.jinniu2.jinniu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jinniu2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
